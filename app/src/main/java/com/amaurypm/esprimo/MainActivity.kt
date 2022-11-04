e21a7bca7086d7c0789fd894494f459a325a0aa3
AmauryPM <amaury.matsumura@gmail.com>
2022-11-04 12:57:20 -0600

    Splash screen e ícono añadidos

--- app/src/main/java/com/amaurypm/esprimo/MainActivity.kt
+++ app/src/main/java/com/amaurypm/esprimo/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.amaurypm.esprimo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
-import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.amaurypm.esprimo.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,21 +20,42 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*val tvPrueba1 = findViewById<TextView>(R.id.tvPrueba1)
-        tvPrueba1.text = "Amaury"*/
-
-
-
     }
 
     fun click(view: View) {
-        val numero = binding.etNumero.text.toString().toInt()
 
-        if(esPrimo(numero)){
-            binding.tvResultado.text = "El número $numero sí es primo"
-        }else{
-            binding.tvResultado.text = "El número $numero no es primo"
+        when(view.id){
+
+            R.id.btnVerificar -> {
+                if(binding.etNumero.text.isNotEmpty()) {
+
+                    val numero = binding.etNumero.text.toString().toInt()
+
+                    if (esPrimo(numero)) {
+                        binding.tvResultado.text = getString(R.string.si_primo, numero, "!")
+                    }else{
+                        binding.tvResultado.text = getString(R.string.no_primo, numero)
+                    }
+                }else{
+                    Toast.makeText(this@MainActivity, resources.getString(R.string.ingresa_valor), Toast.LENGTH_SHORT).show()
+                    binding.etNumero.error = getString(R.string.valor_requerido)
+                }
+            }
+
+            R.id.btnEs -> {
+                setLocale("es")
+            }
+
+            R.id.btnEn -> {
+                setLocale("en")
+            }
+
+            R.id.btnConf -> {
+                startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS));
+            }
+
         }
+
     }
 
     fun esPrimo(numero: Int): Boolean{
@@ -38,5 +64,21 @@ class MainActivity : AppCompatActivity() {
             if(numero % i == 0) return false
         }
         return true
+    }
+
+    fun setLocale(codigoIdioma: String){
+        val config = resources.configuration
+        val locale = Locale(codigoIdioma)
+        Locale.setDefault(locale)
+
+        config.setLocale(locale)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+            createConfigurationContext(config)
+
+        resources.updateConfiguration(config, resources.displayMetrics)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
